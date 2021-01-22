d263c8fdd83d78cc78d87747f14ce7cc7e250857
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2021-01-22 17:52:09 +0700

    Finish Q3,4,5,6

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -19,3 +19,41 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double A[][N]){
+	for(int i = 0; i < N; i++){
+		cout << "Row " << i+1 << ": ";
+		for(int j = 0; j < N; j++){
+			cin >> A[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double matrix[][N], bool res[][N]){
+	int j, k = 0;
+	for(j = 0; j < N; j++){
+		for(k = 0; k < N; k++){
+			double current = matrix[j][k];
+			res[j][k] =  0;
+
+			if(j > 0 && j < N - 1 && k > 0 && k < N - 1){
+				if(current >= matrix[j][k-1] && current >= matrix[j][k+1] && 
+					current >= matrix[j+1][k] && current >= matrix[j-1][k]){
+					res[j][k] =  1;
+				}
+			} else {
+				res[j][k] = 0;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool res[][N]){
+	for(int j = 0; j < N; j++){
+		for (int k = 0; k < N; k++){
+			cout << res[j][k] << " ";
+		}
+		cout << "\n";
+	}
+}
+ 
